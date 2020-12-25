62563506ee38617efe8ff1166ba3437123eb59c9
tzhengliin <47517411+tzhengliin@users.noreply.github.com>
2020-12-25 11:32:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function showMobileMenu() {
+    let mobileMenu = document.querySelector("#mobile-menu");
+    mobileMenu.style.display = "block";
+}
+
+function closeMobileMenu() {
+    let mobileMenu = document.querySelector("#mobile-menu");
+    mobileMenu.style.display = "none";
+}
